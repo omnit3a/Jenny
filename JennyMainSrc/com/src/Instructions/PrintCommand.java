d9329c18655a4d5821d5d1a73c56fe0d0127a64a
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-02 10:59:29 -0500

    Update PrintCommand.java

--- JennyMainSrc/com/src/Instructions/PrintCommand.java
+++ JennyMainSrc/com/src/Instructions/PrintCommand.java
@@ -8,11 +8,11 @@ public class PrintCommand {
         Main.OPC++;
         try{
             String msg = Main.st.nextToken();
-            if (msg.endsWith("^")) {
-                msg = msg.stripLeading();
-                msg = msg.replace('^', ' ');
-                msg = msg.strip();
-                msg = msg.replace("_", " ");
+            if (msg.endsWith("^")) {            //this adds a new line character to the operand if it ends with "^"
+                msg = msg.stripLeading();      
+                msg = msg.replace('^', ' ');    //this removes the "^" character
+                msg = msg.strip();              //this removes the trailing spaces
+                msg = msg.replace("_", " ");    //and finally, this adds in the spaces to the operand
                 System.out.println(msg);
             } else {
                 msg = msg.stripLeading();
