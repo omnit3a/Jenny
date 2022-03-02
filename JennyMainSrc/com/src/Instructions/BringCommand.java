65cdcacf13b1be84cd9ca5a50908c2a1a85a9bc4
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-02 10:47:55 -0500

    Update BringCommand.java

--- JennyMainSrc/com/src/Instructions/BringCommand.java
+++ JennyMainSrc/com/src/Instructions/BringCommand.java
@@ -11,11 +11,11 @@ public class BringCommand {
             int[] elements = new int[Main.vars.size()];
             int tempA = Main.vars.pop();
             int tempB = 0;
-            for (int i = Main.vars.size() ; i > Main.operandA+1 ; i--){
+            for (int i = Main.vars.size() ; i > Main.operandA+1 ; i--){     //this saves the elements need to be popped to access the specified stack entry...
                 elements[i] = Main.vars.pop();
             }
             tempB = Main.vars.pop();
-            for (int i = 0 ; i < elements.length ; i++){
+            for (int i = 0 ; i < elements.length ; i++){                    //and this replaces those elements that were popped
                 Main.vars.push(elements[i]);
             }
             Main.vars.push(tempB);
