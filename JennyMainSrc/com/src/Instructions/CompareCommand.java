6701f77e00c210b1e43170b133a6168d1263c855
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-02 10:48:23 -0500

    Update CompareCommand.java

--- JennyMainSrc/com/src/Instructions/CompareCommand.java
+++ JennyMainSrc/com/src/Instructions/CompareCommand.java
@@ -8,7 +8,7 @@ public class CompareCommand {
         Main.OPC++;
         int A = Main.vars.pop();
         int B = Main.vars.pop();
-        Main.ZeroFlag = A != B;
+        Main.ZeroFlag = A != B;                 //this compares the top 2 elements on the stack
         Main.vars.push(B);
         Main.vars.push(A);
     }
