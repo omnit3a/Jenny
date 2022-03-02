3bf21aa8df9ad807ea4472d2cd1d7d9d1490cd38
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-02 10:57:56 -0500

    Update PopCommand.java

--- JennyMainSrc/com/src/Instructions/PopCommand.java
+++ JennyMainSrc/com/src/Instructions/PopCommand.java
@@ -6,6 +6,6 @@ public class PopCommand {
     public static void popCommand(){
         Main.PC++;
         Main.OPC++;
-        Main.vars.pop();
+        Main.vars.pop();            //this removes the value on top of the stack, and doesnt push it back on
     }
 }
