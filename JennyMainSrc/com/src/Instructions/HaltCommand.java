4d3017ae6ff980c79972e48cbac984280028894e
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-02 10:50:23 -0500

    Update HaltCommand.java

--- JennyMainSrc/com/src/Instructions/HaltCommand.java
+++ JennyMainSrc/com/src/Instructions/HaltCommand.java
@@ -5,7 +5,7 @@ import com.src.Main;
 public class HaltCommand {
     public static void haltCommand(){
         try{
-            Main.operandA = Integer.parseInt(Main.st.nextToken());
+            Main.operandA = Integer.parseInt(Main.st.nextToken());          //this gets the exit code for the exit command
             System.exit(Main.operandA);
         } catch (Exception e){
             System.out.println(Main.PC+": Non-Numeric Operand or Exit code overflow");
