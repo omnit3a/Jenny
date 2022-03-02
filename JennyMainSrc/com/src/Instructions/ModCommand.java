3ba6d395753e549f4b2e5f3879f545d75cbca09f
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-02 10:54:54 -0500

    Update ModCommand.java

--- JennyMainSrc/com/src/Instructions/ModCommand.java
+++ JennyMainSrc/com/src/Instructions/ModCommand.java
@@ -7,9 +7,9 @@ public class ModCommand {
         Main.PC++;
         Main.OPC++;
         try {
-            int a = Main.vars.pop();
+            int a = Main.vars.pop();                
             int b = Main.vars.pop();
-            Main.vars.push(b%a);
+            Main.vars.push(b%a);                    //this pops the top 2 values from the stack and applies the modulo operation using reverse polish notation, then pushes the sum
         } catch (Exception e){
             System.out.println(Main.PC+": Arithmetic Error");
             System.exit(253);
