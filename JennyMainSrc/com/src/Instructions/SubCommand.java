26d0141a5ce0efd3e5c435838ed33bc9aad1c7c0
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-02 11:01:56 -0500

    Update SubCommand.java

--- JennyMainSrc/com/src/Instructions/SubCommand.java
+++ JennyMainSrc/com/src/Instructions/SubCommand.java
@@ -9,7 +9,7 @@ public class SubCommand {
         try {
             int a = Main.vars.pop();
             int b = Main.vars.pop();
-            Main.vars.push(b-a);
+            Main.vars.push(b-a);        //this pops the top two values of the stack, subtracts them using reverse polish notation, then pushes the sum onto the stack
         } catch (Exception e){
             System.out.println(Main.PC+": Arithmetic Error");
             System.exit(253);
