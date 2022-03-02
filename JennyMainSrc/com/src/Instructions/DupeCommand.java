2da9197036448fa0d61e47e48676c11090a05d28
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-02 10:49:30 -0500

    Update DupeCommand.java

--- JennyMainSrc/com/src/Instructions/DupeCommand.java
+++ JennyMainSrc/com/src/Instructions/DupeCommand.java
@@ -6,7 +6,7 @@ public class DupeCommand {
     public static void dupeCommand() {
         try{
             int tempA = Main.vars.pop();
-            Main.vars.push(tempA);
+            Main.vars.push(tempA);              //this duplicates the top value on the stack
             Main.vars.push(tempA);
         } catch (Exception e){
             System.out.println("No stack entry");
