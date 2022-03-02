b69ab7a77193fe0d65c5ceb474b5004b76588bb1
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-03-02 10:57:29 -0500

    Update PeekPPCommand.java

--- JennyMainSrc/com/src/Instructions/PeekPPCommand.java
+++ JennyMainSrc/com/src/Instructions/PeekPPCommand.java
@@ -7,7 +7,7 @@ public class PeekPPCommand {
         Main.PC++;
         Main.OPC++;
         try {
-            int a = Main.vars.pop();
+            int a = Main.vars.pop();        //this pops the value on the top of the stack, increments it, then pushes it back onto the stack
             a++;
             Main.vars.push(a);
         } catch (Exception e){
